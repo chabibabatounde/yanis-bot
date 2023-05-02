924ed8d54411a6c0962e65e21ddff97592a81b11
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2023-05-02 23:08:49 +0200

    mise en ligne

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,8 +11,8 @@
 
 module.exports.bootstrap = async function() {
     const qrcode = require('qrcode-terminal');
-    const { Client } = require('whatsapp-web.js');
-    const client = new Client();
+    const fs = require('fs');
+    const { Client, RemoteAuth, LegacySessionAuth, LocalAuth } = require('whatsapp-web.js');
 
     sails.config.globals.yanis_sessions = {
         Celtis_Benin: undefined,
@@ -22,7 +22,35 @@ module.exports.bootstrap = async function() {
 
     for (let i = 0; i < Object.keys(sails.config.globals.yanis_sessions).length; i++) {
         const key = Object.keys(sails.config.globals.yanis_sessions)[i];
-        sails.config.globals.yanis_sessions[key] = new Client();
+
+        /* =========================================CLASSIC================================================= */
+
+        //sails.config.globals.yanis_sessions[key] = new Client();
+
+        /* =========================================MONGO================================================= */
+
+
+        /* ==================================LegacySessionAuth=============================================== */
+        /*
+        let SESSION_FILE_PATH = './session_' + key + '.json';
+        let sessionData;
+        if (fs.existsSync(SESSION_FILE_PATH)) {
+            sessionData = require(SESSION_FILE_PATH);
+        }
+        sails.config.globals.yanis_sessions[key] = new Client({
+            authStrategy: new LegacySessionAuth({
+                session: sessionData
+            })
+        });
+        sails.config.globals.yanis_sessions[key].on('authenticated', (session) => {
+            sessionData = session;
+            fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        });
+        /* ======================================COMMON===================================================== */
         sails.config.globals.yanis_sessions[key].initialize();
         sails.config.globals.yanis_sessions[key].on('qr', qr => {
             console.log(key);
@@ -35,11 +63,15 @@ module.exports.bootstrap = async function() {
             let replyString = "Hello 👋🏼, \n";
             replyString += "Je suis Yanis😊, un robot chargé de vous envoyer automatiquement des méssages via Whatsapp.\n";
             replyString += "Je ne suis malheureusement pas en mesure de traiter votre méssage.😕\n";
-            replyString += "Si vous avez reçu précedement un message venant de moi, c'est que vous ne devriez normalement pas y répondre. 😇\n";
+            replyString += "Si vous avez reçu précedement un message venant de moi, c'est que vous ne devriez normalement pas y répondre 😇. \n";
             replyString += "\n 🖥️ _YANIS est un produit développé par https://rodolpho-babatounde.net/_ \n";
-            message.reply(replyString);
-            //console.log(message.body);
-            //sails.helpers.recevemessage(message)
+            let mgs_reply = "Hello Hello 👋🏼!\nJe suis Yanis, un robot whatsapp développé pour vous envoyer automatiquement des messages. ";
+            mgs_reply += "Je ne suis malheuresement pas en mesure de traiter votre méssage et y répondre convenablement😕. ";
+            mgs_reply += "Néanmoins, je ferai de mon mieux pour transmettre votre message à qui de droit😇.";
+            mgs_reply += "\n\n🎊😊 Yacoco!";
+            replyString += "\n 🖥️ _Yanis est un service développé par https://rodolpho-babatounde.net/_";
+            message.reply(mgs_reply);
+            //message.reply(replyString);
         });
     }
 };
